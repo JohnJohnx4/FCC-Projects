d42f8397e24d448d7cfa1ea2213579229dac9572
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-04-01 00:52:13 -0400

    added homepage, edited konami file

--- Konami-Code/app.js
+++ Konami-Code/app.js
@@ -1,25 +1,21 @@
-const konamiCode = () => {
-    window.addEventListener("keydown", checkKeyPress, false);
-    
-    let keyCombo = "";
-    let comboing = false;
-    let konamicode = "3838404037393739666513";
-    function checkKeyPress(key) {
-        keyCombo += key.keyCode.toString();
-        console.log(keyCombo);
-        if (keyCombo === konamicode) {
-            alert("You did it!");
-            window.location.href = "https://en.wikipedia.org/wiki/Konami_Code";
-        }
-        if (comboing === false) {
-            comboing = true;
-            this.setTimeout(() => {
-                keyCombo = "";
-                console.log("timed out", keyCombo);
-                comboing = false;
-            }, 2500);
-        }
-    }
-}
+window.addEventListener("keydown", checkKeyPress, false);
 
-export default konamiCode;
+let keyCombo = "";
+let comboing = false;
+let konamicode = "3838404037393739666513";
+function checkKeyPress(key) {
+  keyCombo += key.keyCode.toString();
+  console.log(keyCombo);
+  if (keyCombo === konamicode) {
+    alert("You did it!");
+    window.location.href = "https://en.wikipedia.org/wiki/Konami_Code";
+  }
+  if (comboing === false) {
+    comboing = true;
+    this.setTimeout(() => {
+      keyCombo = "";
+      console.log("timed out", keyCombo);
+      comboing = false;
+    }, 2500);
+  }
+}
