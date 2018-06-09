5f8bd1b1e3750d7bc94e7b6346ec444161d873ea
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-06-09 01:54:18 -0400

    progress

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -3,20 +3,13 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0]
-];
-
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
-    for (let y = 0; y < m.length, ++y) {
+    for (let y = 0; y < m.length; ++y) {
         for (let x = 0; x < m[y].length; ++x) {
             if (m[y][x] !== 0 &&
-            arena[y + o.y] &&
-            arena[y + o.y][x + o.x] !== 0) {
+            (arena[y + o.y] &&
+            arena[y + o.y][x + o.x]) !== 0) {
                 return true;
             }
         }
@@ -26,16 +19,63 @@ function collide(arena, player) {
 
 function createMatrix(w, h) {
     const matrix = [];
-    white(h--) {
+    while(h--) {
         matrix.push(new Array(w).fill(0))
     }
     return matrix;
+}
+
+function createPiece(type) {
+        if (type === 'I') {
+            return [
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+            ];
+        } else if (type === 'L') {
+            return [
+                [0, 2, 0],
+                [0, 2, 0],
+                [0, 2, 2],
+            ];
+        } else if (type === 'J') {
+            return [
+                [0, 3, 0],
+                [0, 3, 0],
+                [3, 3, 0],
+            ];
+        } else if (type === 'O') {
+            return [
+                [4, 4],
+                [4, 4],
+            ];
+        } else if (type === 'Z') {
+            return [
+                [5, 5, 0],
+                [0, 5, 5],
+                [0, 0, 0],
+            ];
+        } else if (type === 'S') {
+            return [
+                [0, 6, 6],
+                [6, 6, 0],
+                [0, 0, 0],
+            ];
+        } else if (type === 'T') {
+            return [
+                [0, 7, 0],
+                [7, 7, 7],
+                [0, 0, 0],
+            ];
+        }
 }
 
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
     
+    drawMatrix(arena, {x: 0, y: 0});
     drawMatrix(player.matrix, player.pos);
 }
 
@@ -62,7 +102,68 @@ function merge(arena, player) {
 
 function playerDrop() {
     player.pos.y++;
-    dropCounter=0;
+    if (collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        playerReset();
+        player.pos.y = 0;
+    }
+    dropCounter = 0;
+}
+
+function playerMove(dir) {
+    player.pos.x += dir;
+    if (collide(arena, player)) {
+        player.pos.x -= dir;
+    }
+}
+
+function playerReset() {
+    const pieces = 'ILJOTSZ';
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0
+    player.pos.x = (arena[0].length / 2 | 0) -
+    (palyer.matrix[0].length / 2 | 0);
+
+    if (collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+    }
+}
+
+function playerRotate(dir) {
+    const pos = player.pos;
+    let offset = 1
+    rotate(player.matrix, dir);
+    while(collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0]. length) {
+            rotate(player.matrix, -dir);
+            palyer.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; x++) {
+            [
+
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
 }
 
 let dropCounter = 0;
@@ -88,17 +189,21 @@ const arena = createMatrix(12, 20);
 
 const player = {
     pos: {x:5, y:5},
-    matrix: matrix,
+    matrix: createPiece('I'),
 }
 
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
-        player.pos.x--;
+        playerMove(-1);
     } else if (event.keyCode === 39) {
-        player.pos.x++;
+        playerMove(1);
     } else if (event.keyCode === 40) {
         playerDrop();
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
     }
-})
+});
 
 update();
