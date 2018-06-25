297ea7cf8cf115e36d3dceaaec562f228ad0c757
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-06-25 00:17:44 -0400

    added Less, smooth scrolling, and better color scheme

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
